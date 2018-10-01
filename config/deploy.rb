427c883ab71576f2ecbbc5851fa0a85cbd9fd0fa
srng <shaneryanglass88@gmail.com>
2018-10-01 12:11:10 -0700

    puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,7 @@ set :deploy_to, "/home/rosewall/rosewall"
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 append :linked_files, "config/master.key"
+append :rbenv_map_bins, 'puma', 'pumactl'
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
