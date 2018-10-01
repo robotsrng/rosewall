c981299326a2c68e8bbbc31bdf819116aecb5b3b
srng <shaneryanglass88@gmail.com>
2018-09-30 19:15:23 -0700

    change cap dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :user, "rosewall"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
- set :deploy_to, "/home/rosewall/"
+ set :deploy_to, "/home/rosewall/rosewall"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
