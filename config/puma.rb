6ae79c201b06a10ad3a4880092111ca719968711
srng <shaneryanglass88@gmail.com>
2018-10-01 09:33:55 -0700

    cap

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,7 @@
 #
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
-bind "unix://home/rosewall/rosewall/current/tmp/sockets/puma.sock"
+bind "unix:/home/rosewall/rosewall/current/tmp/sockets/puma.sock"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
