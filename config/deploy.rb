8815f244dc1c6ec7a86a62b0cd00ca00da408d89
srng <shaneryanglass88@gmail.com>
2018-09-30 18:43:45 -0700

    port change

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,3 +38,4 @@ set :user, "rosewall"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+append :linked_files, "config/master.key"
