411452755db1947a4c65f1a1053d5ef00c44afcc
srng <shaneryanglass88@gmail.com>
2018-10-02 09:57:12 -0700

    changed let to var in js

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -9,19 +9,19 @@ $(document).on('turbolinks:load', function () {
     });
 
     $(document).on('click', '#cone-calc-button', function () {
-        let rds = $('#calc-radius').val();
-        let dpth = $('#calc-depth').val();
+        var rds = $('#calc-radius').val();
+        var dpth = $('#calc-depth').val();
         alert('You require : ' + (((((rds*rds) * dpth) * Math.PI) / 3) / 27 ) + 'cubic yards of material.');
     });
 
     $(document).on('click', '#cylinder-calc-button', function () {
-        let rds = $('#calc-radius').val();
-        let dpth = $('#calc-depth').val();
+        var rds = $('#calc-radius').val();
+        var dpth = $('#calc-depth').val();
         alert('You require : ' + ((((rds*rds) * dpth) * Math.PI) / 27).toString() + ' cubic yards of material.' );
     });
 
     $('#calculator-selector').on('change', function () {
-        let calc_type = $('input[name="calculator-selector"]:checked').val();
+        var calc_type = $('input[name="calculator-selector"]:checked').val();
         if (calc_type.startsWith('square')) {
             $('#calculator-container').html(square_calculator_html);
         } else if (calc_type.startsWith('cone')) {
@@ -34,14 +34,14 @@ $(document).on('turbolinks:load', function () {
     $
 
 
-    let square_calculator_html = '<div class="square-calculator ">\n' +
+    var square_calculator_html = '<div class="square-calculator ">\n' +
         '      <label for="calc-height">Height (ft): </label><input id="calc-height" type="number" name="calc-height"><br>\n' +
         '      <label for="calc-width">Width (ft): </label><input id="calc-width" type="number" name="calc-width"><br>\n' +
         '      <label for="calc-depth">Depth (ft): </label><input id="calc-depth" type="number" name="calc-depth"><br>\n' +
         '      <button type="button" id="square-calc-button">Calculate</button>\n' +
         '    </div>';
 
-    let cone_calculator_html = '<div class="cone-calculator ">\n' +
+    var cone_calculator_html = '<div class="cone-calculator ">\n' +
         '\n' +
         '    <label for="calc-radius">Radius (ft): </label><input id="calc-radius" type="number" name="calc-radius"><br>\n' +
         '    <label for="calc-depth">Depth (ft): </label><input id="calc-depth" type="number" name="calc-depth"><br>\n' +
@@ -49,7 +49,7 @@ $(document).on('turbolinks:load', function () {
         '\n' +
         '  </div>';
 
-    let cylinder_calculator_html = '<div class="cylinder-calculator ">\n' +
+    var cylinder_calculator_html = '<div class="cylinder-calculator ">\n' +
         '\n' +
         '    <label for="calc-radius">Radius (ft): </label><input id="calc-radius" type="number" name="calc-radius"><br>\n' +
         '    <label for="calc-depth">Depth (ft): </label><input id="calc-depth" type="number" name="calc-depth"><br>\n' +
