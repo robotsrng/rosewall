f6d081dfdb7d59b66c15385952c1b3c5be29aec0
srng <shaneryanglass88@gmail.com>
2018-10-03 15:57:52 -0700

    design upgrades

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -4,20 +4,26 @@ $(document).on('turbolinks:load', function () {
         var hght = $('#calc-height').val();
         var wdth = $('#calc-width').val();
         var dpth = $('#calc-depth').val();
-        alert('You require : ' + ( (hght * wdth * dpth) / 27 ) + 'cubic yards of material.');
+        var number = ((hght * wdth * dpth) / 27)
+        var total = Math.round(number * 10) / 10
+        $('.js-calculator-total').html('The total material required is ' + total + ' cubic yards.');
 
     });
 
     $(document).on('click', '#cone-calc-button', function () {
-        var rds = $('#calc-radius').val();
-        var dpth = $('#calc-depth').val();
-        alert('You require : ' + (((((rds*rds) * dpth) * Math.PI) / 3) / 27 ) + 'cubic yards of material.');
+        var rds = $('#calc-radius').val()/3;
+        var dpth = $('#calc-depth').val()/3;
+        var number = 3.14 * (rds * rds) * (dpth/3)
+        var total = Math.round(number * 10) / 10
+        $('.js-calculator-total').html('The total material required is ' + total + ' cubic yards.');
     });
 
     $(document).on('click', '#cylinder-calc-button', function () {
-        var rds = $('#calc-radius').val();
-        var dpth = $('#calc-depth').val();
-        alert('You require : ' + ((((rds*rds) * dpth) * Math.PI) / 27).toString() + ' cubic yards of material.' );
+        var rds = $('#calc-radius').val()/3;
+        var dpth = $('#calc-depth').val()/3;
+        var number = ((((rds * rds) * dpth) * 3.14))
+        var total = Math.round(number * 10) / 10
+        $('.js-calculator-total').html('The total material required is ' + total + ' cubic yards.');
     });
 
     $('#calculator-selector').on('change', function () {
@@ -38,14 +44,14 @@ $(document).on('turbolinks:load', function () {
         '      <label for="calc-height">Height (ft): </label><input id="calc-height" type="number" name="calc-height"><br>\n' +
         '      <label for="calc-width">Width (ft): </label><input id="calc-width" type="number" name="calc-width"><br>\n' +
         '      <label for="calc-depth">Depth (ft): </label><input id="calc-depth" type="number" name="calc-depth"><br>\n' +
-        '      <button type="button" id="square-calc-button">Calculate</button>\n' +
+        '      <button type="button" id="square-calc-button" data-toggle="modal" data-target="#myModal">Calculate</button>\n' +
         '    </div>';
 
     var cone_calculator_html = '<div class="cone-calculator ">\n' +
         '\n' +
         '    <label for="calc-radius">Radius (ft): </label><input id="calc-radius" type="number" name="calc-radius"><br>\n' +
         '    <label for="calc-depth">Depth (ft): </label><input id="calc-depth" type="number" name="calc-depth"><br>\n' +
-        '    <button type="button" id="cone-calc-button">Calculate</button>\n' +
+        '    <button type="button" id="cone-calc-button" data-toggle="modal" data-target="#myModal">Calculate</button>\n' +
         '\n' +
         '  </div>';
 
@@ -53,7 +59,7 @@ $(document).on('turbolinks:load', function () {
         '\n' +
         '    <label for="calc-radius">Radius (ft): </label><input id="calc-radius" type="number" name="calc-radius"><br>\n' +
         '    <label for="calc-depth">Depth (ft): </label><input id="calc-depth" type="number" name="calc-depth"><br>\n' +
-        '    <button type="button" id="cylinder-calc-button">Calculate</button>\n' +
+        '    <button type="button" id="cylinder-calc-button" data-toggle="modal" data-target="#myModal">Calculate</button>\n' +
         '\n' +
         '  </div>';
 });
