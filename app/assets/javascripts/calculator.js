26ae4cd3f875c26e6807d9f3d296750a8f675db8
srng <shaneryanglass88@gmail.com>
2018-10-05 20:14:11 -0700

    design changes

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -11,16 +11,16 @@ $(document).on('turbolinks:load', function () {
     });
 
     $(document).on('click', '#cone-calc-button', function () {
-        var rds = $('#calc-radius').val()/3;
-        var dpth = $('#calc-depth').val()/3;
-        var number = 3.14 * (rds * rds) * (dpth/3)
+        var rds = $('#calc-radius').val() / 3;
+        var dpth = $('#calc-depth').val() / 3;
+        var number = 3.14 * (rds * rds) * (dpth / 3)
         var total = Math.round(number * 10) / 10
         $('.js-calculator-total').html('The total material required is ' + total + ' cubic yards.');
     });
 
     $(document).on('click', '#cylinder-calc-button', function () {
-        var rds = $('#calc-radius').val()/3;
-        var dpth = $('#calc-depth').val()/3;
+        var rds = $('#calc-radius').val() / 3;
+        var dpth = $('#calc-depth').val() / 3;
         var number = ((((rds * rds) * dpth) * 3.14))
         var total = Math.round(number * 10) / 10
         $('.js-calculator-total').html('The total material required is ' + total + ' cubic yards.');
@@ -40,26 +40,51 @@ $(document).on('turbolinks:load', function () {
     $
 
 
-    var square_calculator_html = '<div class="square-calculator ">\n' +
-        '      <label for="calc-height">Height (ft): </label><input id="calc-height" type="number" name="calc-height"><br>\n' +
-        '      <label for="calc-width">Width (ft): </label><input id="calc-width" type="number" name="calc-width"><br>\n' +
-        '      <label for="calc-depth">Depth (ft): </label><input id="calc-depth" type="number" name="calc-depth"><br>\n' +
-        '      <button type="button" id="square-calc-button" data-toggle="modal" data-target="#myModal">Calculate</button>\n' +
-        '    </div>';
+    var square_calculator_html =
 
-    var cone_calculator_html = '<div class="cone-calculator ">\n' +
-        '\n' +
-        '    <label for="calc-radius">Radius (ft): </label><input id="calc-radius" type="number" name="calc-radius"><br>\n' +
-        '    <label for="calc-depth">Depth (ft): </label><input id="calc-depth" type="number" name="calc-depth"><br>\n' +
-        '    <button type="button" id="cone-calc-button" data-toggle="modal" data-target="#myModal">Calculate</button>\n' +
-        '\n' +
-        '  </div>';
+        '<div class="square-calculator ">\n' +
+        '<table class="table">\n' +
+        '  <tr class="row">\n' +
+        '      <td class="col-md-2"><label for="calc-height">Height (ft): </label></td><td class="col-md-2"><input id="calc-height" type="number" name="calc-height"></td>\n' +
+        '  </tr>\n' +
+        '  <tr class="row">\n' +
+        '    <td class="col-md-2"><label for="calc-width">Width (ft): </label></td><td class="col-md-2"><input id="calc-width" type="number" name="calc-width"></td>\n' +
+        '  </tr>\n' +
+        '  <tr class="row">\n' +
+        '    <td class="col-md-2"><label for="calc-depth">Depth (ft): </label></td><td class="col-md-2"><input id="calc-depth" type="number" name="calc-depth"></td>\n' +
+        '  </tr>\n' +
+        '  <tr class="row">\n' +
+        '    <td class="col-md-2"><button type="button" id="square-calc-button" data-toggle="modal" data-target="#myModal">Calculate</button></td>\n' +
+        '  </tr>\n  ' +
+        '</table>\n' +
+        '</div>';
+
+    var cone_calculator_html =
+        '<div class="cone-calculator ">\n' +
+        '<table class="table">\n' +
+        '  <tr class="row">\n' +
+        '    <td class="col-md-2"><label for="calc-radius">Radius (ft): </label></td><td class="col-md-2"><input id="calc-radius" type="number" name="calc-radius"></td>\n' +
+        '  </tr>\n    ' +
+        '  <tr class="row">\n' +
+        '    <td class="col-md-2"><label for="calc-depth">Depth (ft): </label></td><td class="col-md-2"><input id="calc-depth" type="number" name="calc-depth"></td>\n' +
+        '  </tr>\n    ' +
+        '  <tr class="row">' +
+        '    <td class="col-md-2"><button type="button" id="cone-calc-button" data-toggle="modal" data-target="#myModal">Calculate</button></td>\n' +
+        '  </tr>\n' +
+        '</table>\n ' +
+        '</div>';
 
     var cylinder_calculator_html = '<div class="cylinder-calculator ">\n' +
-        '\n' +
-        '    <label for="calc-radius">Radius (ft): </label><input id="calc-radius" type="number" name="calc-radius"><br>\n' +
-        '    <label for="calc-depth">Depth (ft): </label><input id="calc-depth" type="number" name="calc-depth"><br>\n' +
-        '    <button type="button" id="cylinder-calc-button" data-toggle="modal" data-target="#myModal">Calculate</button>\n' +
-        '\n' +
-        '  </div>';
+        '<table class="table">\n' +
+        '  <tr class="row">\n' +
+        '    <td class="col-md-2"><label for="calc-radius">Radius (ft): </label></td><td class="col-md-2"><input id="calc-radius" type="number" name="calc-radius"></td>\n' +
+        '  </tr>\n    ' +
+        '  <tr class="row">\n' +
+        '    <td class="col-md-2"><label for="calc-depth">Depth (ft): </label></td><td class="col-md-2"><input id="calc-depth" type="number" name="calc-depth"></td>\n' +
+        '  </tr>\n    ' +
+        '  <tr class="row">' +
+        '    <td class="col-md-2"><button type="button" id="cylinder-calc-button" data-toggle="modal" data-target="#myModal">Calculate</button></td>\n' +
+        '  </tr>\n' +
+        '</table>\n ' +
+        '</div>';
 });
